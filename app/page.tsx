bf216132bfd2deb46b5f91756d498025bff0c6db
Nicholas Moreland <nmoreland18@outlook.com>
2023-11-16 20:10:15 -0600

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <main className="pb-40 pt-10">
+        <section className="px-20 py-10 h-96">
+          <h1 className="text-5xl font-bold">Hi, I am Nicholas Moreland</h1>
+          <p className="text-2xl pt-4">
+            I am a software engineer with a passion for building things.
+          </p>
+        </section>
+        <section className="px-20 py-10 h-96 bg-gray-100">
+          <h1 className="text-5xl font-bold">Skills</h1>
+          <p className="text-2xl pt-4">
+            I am a software engineer with a passion for building things.
+          </p>
+        </section>
+        <section className="px-20 py-10 h-96">
+          <h1 className="text-5xl font-bold">Work Experience</h1>
+          <p className="text-2xl pt-4">
+            I am a software engineer with a passion for building things.
+          </p>
+        </section>
+        <section className="px-20 py-10 h-96 bg-gray-100">
+          <h1 className="text-5xl font-bold">Projects</h1>
+          <p className="text-2xl pt-4">
+            I am a software engineer with a passion for building things.
+          </p>
+        </section>
+        <section className="px-20 py-10 h-96">
+          <h1 className="text-5xl font-bold">Education</h1>
+          <p className="text-2xl pt-4">
+            I am a software engineer with a passion for building things.
+          </p>
+        </section>
+        <section className="px-20 py-10 h-96 bg-gray-100">
+          <h1 className="text-5xl font-bold">Certifications</h1>
+          <p className="text-2xl pt-4">
+            I am a software engineer with a passion for building things.
+          </p>
+        </section>
+      </main>
+    </>
+  );
+}
